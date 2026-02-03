3bc3bc09e5965ac73d90370bd607dd346065b595
Joey Tan <104960744+joeyltan@users.noreply.github.com>
2026-02-03 18:36:03 -0500

    add: comments on your-rides page + client

--- app/your-rides/page.tsx
+++ app/your-rides/page.tsx
@@ -6,13 +6,13 @@ import YourRidesClient from "./your-rides-client";
 export default async function DashboardPage() {
   // server-side: verify httpOnly JWT w/ updated helper, chat helped with this sorry guys desperate times
   const netId = await getUserNetIdFromCookies();
+  // if netid is null then redirect to CAS login, and if successful login, redirects back to your rides page
   if (!netId) {
     redirect(`/api/auth/cas-login?next=${encodeURIComponent("/your-rides")}`);
   }
 
-  const numOwnedRides = 100
+  // Fetch owned rides matching the user that are open
   const ownedRides = await prisma.ride.findMany({
-    take: numOwnedRides,
     where: { ownerNetId: netId, isClosed: false },
     orderBy: { startTime: "desc" },
   });
@@ -24,6 +24,7 @@ export default async function DashboardPage() {
   return (
     <YourRidesClient
       ownedRides={ownedRides}
+      // note: though these are passed in they are not shown in the your rides page (hidebookmark set to false in the client)
       bookmarkedRideIds={bookmarkedRideIds}
     />
   );
