9e61618776a939732035f949f21d27e431ab6d97
niksaitov <nik.saitov.04@gmail.com>
2026-02-10 18:49:02 -0500

    refactor(logout-api): use private functions, better readability & comments

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,36 +1,23 @@
 import { NextResponse } from "next/server";
 
-function getBaseUrl(): string {
-  if (process.env.NEXTAUTH_URL) return process.env.NEXTAUTH_URL;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return "http://localhost:3000";
-}
+import { withApiErrorHandler } from "@/lib/infra";
 
-export async function GET() {
-  
-  const baseUrl = getBaseUrl();
+import { getYideshareUrl } from "../_url";
 
-  if (process.env.NODE_ENV === "production")
-  {
+async function handleLogout(req: Request) {
+  const yideshareUrl = getYideshareUrl(req);
+  // Redirect to base by default; cannot destroy SSO session if not in production
+  let response = NextResponse.redirect(yideshareUrl);
 
-    // this will not redirect to base on localhost
-    const casLogoutUrl = `https://secure.its.yale.edu/cas/logout?service=${encodeURIComponent(baseUrl)}`;
-    const response = NextResponse.redirect(casLogoutUrl);
-
-    // currently only storing auth cookies
-    response.cookies.delete("auth");
-
-    return response;
-  } 
-  else 
-  {
-    // just redirect to base, cannot gracefully destroy sso session
-    const response = NextResponse.redirect(baseUrl);
-    response.cookies.delete("auth");
-
-    return response;
+  if (process.env.NODE_ENV === "production") {
+    const casLogoutUrl = `https://secure.its.yale.edu/cas/logout
+      ?service=${encodeURIComponent(yideshareUrl)}`;
+    // Use logout service to destroy SSO session
+    response = NextResponse.redirect(casLogoutUrl);
   }
 
-  
-  
+  response.cookies.delete("auth");
+  return response;
 }
+
+export const GET = withApiErrorHandler(handleLogout);
