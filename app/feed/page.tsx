a1bd4835f91133c4180d5fcd1b13081950fe292a
niksaitov <nik.saitov.04@gmail.com>
2026-04-16 20:55:22 -0400

    style(feed page): fit main page to style

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,27 +1,24 @@
-import FeedPageClient from "./feed-page-client";
 import { findBookmarkedRides, findManyRides } from "@/lib/db";
 import { getUserNetIdFromCookies } from "@/lib/cookies";
 
+import FeedPageClient from "./FeedPageClient";
 
 export default async function Feed() {
-  /* ----------------  auth  ---------------- */
-    const netId = await getUserNetIdFromCookies();
+  const netId = await getUserNetIdFromCookies();
 
-    if (netId === null) {
-      return <div>Please log in to view your rides.</div>;
-    }
+  if (netId === null) {
+    return <div>Please log in to view your rides.</div>;
+  }
 
-    /* ----------------  data  ---------------- */
-    const numInitialRides = 500;
-    const initialRides = await findManyRides(numInitialRides);
-    const bookmarks = await findBookmarkedRides(netId);
-    const bookmarkedRideIds = bookmarks.map((b) => b.ride.rideId);
+  const numInitialRides = 500;
+  const initialRides = await findManyRides(numInitialRides);
+  const bookmarks = await findBookmarkedRides(netId);
+  const bookmarkedRideIds = bookmarks.map((b) => b.ride.rideId);
 
-    /* ----------------  UI  ---------------- */
-    return (
-      <FeedPageClient
-        initialRides={initialRides}
-        bookmarkedRideIds={bookmarkedRideIds}
-      />
-    );
+  return (
+    <FeedPageClient
+      initialRides={initialRides}
+      bookmarkedRideIds={bookmarkedRideIds}
+    />
+  );
 }
